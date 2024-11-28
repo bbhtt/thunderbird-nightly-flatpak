3200bc5b54494a86da905444d7c32765c4b1e98d
bbhtt <bbhtt.zn0i8@slmail.me>
2024-11-28 20:39:19 +0530

    Fix the updater script

--- updater.sh
+++ updater.sh
@@ -4,18 +4,18 @@ set -e
 
 update () {
 
-	relnum=$(awk '/tar.bz2/{print NR; exit}' "$MANIFEST_PATH")
+	relnum=$(awk '/tar.*/{print NR; exit}' "$MANIFEST_PATH")
 	vernum=$(awk '/VERSION:/{print NR; exit}' "$MANIFEST_PATH")
-	shanum=$(awk '/sha256/{print NR}' "$MANIFEST_PATH"|tail -n1)
-	oldrelease=$(sed "${relnum}!d" "$MANIFEST_PATH"|sed -n '{s|.*/thunderbird-\(.*\)\.tar.bz2|\1|p;q;}')
-	newrelease=$(wget -q --spider -S --max-redirect 0 "https://download.mozilla.org/?product=thunderbird-nightly-latest-SSL&os=linux64&lang=en-US" 2>&1 | sed -n '/Location: /{s|.*/thunderbird-\(.*\)\.tar.*|\1|p;q;}')
+	shanum=$(awk '/sha256/{print NR}' "$MANIFEST_PATH"|tail -n 1)
+	oldrelease=$(sed "${relnum}!d" "$MANIFEST_PATH"|sed -n '{s|.*/thunderbird-\(.*\)\.tar.*|\1|p;q;}')
+	newrelease=$(curl -I -s -L "https://download.mozilla.org/?product=thunderbird-nightly-latest-SSL&os=linux64&lang=en-US" 2>&1 | sed -n '/Location: /{s|.*/thunderbird-\(.*\)\.tar.*|\1|p;q;}')
 	sed -i "${relnum}s/$oldrelease/$newrelease/g" "$MANIFEST_PATH"
-	updrelease=$(sed "${relnum}!d" "$MANIFEST_PATH"|sed -n '{s|.*/thunderbird-\(.*\)\.tar.bz2|\1|p;q;}')
+	updrelease=$(sed "${relnum}!d" "$MANIFEST_PATH"|sed -n '{s|.*/thunderbird-\(.*\)\.tar.*|\1|p;q;}')
 	versionold=$(echo "$oldrelease"|head -c 7)
 	versionnew=$(echo "$updrelease"|head -c 7)
 	sed -i "${vernum}s/VERSION: $versionold/VERSION: $versionnew/g" "$MANIFEST_PATH"
-	wget -q https://download-installer.cdn.mozilla.net/pub/thunderbird/nightly/latest-comm-central/thunderbird-"$updrelease".checksums
-	shanew=$(grep "$updrelease".tar.bz2 < thunderbird-"$updrelease".checksums|grep -v asc|grep -v sha512|cut -d " " -f1)
+	curl -s -O https://download-installer.cdn.mozilla.net/pub/thunderbird/nightly/latest-comm-central/thunderbird-"$updrelease".checksums
+	shanew=$(grep "$updrelease".tar.xz < thunderbird-"$updrelease".checksums|grep -v asc|grep -v sha512|cut -d " " -f1)
 	shaold=$(sed "${shanum}!d" "$MANIFEST_PATH"|cut -d: -f2|sed 's/^[ \t]*//;s/[ \t]*$//')
 	sed -i "${shanum}s/$shaold/$shanew/g" "$MANIFEST_PATH"
 	rm -- *.checksums
